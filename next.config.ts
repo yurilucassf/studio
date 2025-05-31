52b3f01d6be76777141c14445ff5921771346957
Yuri Lucas <yurilucassfm@gmail.com>
2025-05-31 23:43:23 +0000

    na pagina de catalogo de livros deu o seguinte erro: Unhandled Runtime Error
    Error: Invalid src prop (https://m.media-amazon.com/images/I/71u9sJm9q+L.AC_UF1000,1000_QL80.jpg) on `next/image`, hostname "m.media-amazon.com" is not configured under images in your `next.config.js`
    See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host
    
    src/components/catalog/book-card.tsx (43:11) @ BookCard
    
      41 |       <CardHeader className="p-4 relative">
      42 |         <div className="aspect-[2/3] w-full relative mb-3 rounded-md overflow-hidden bg-muted">
    > 43 |           <Image
         |           ^
      44 |             src={coverImage}
      45 |             alt={`Capa do livro ${book.title}`}
      46 |             layout="fill"
    Call Stack
    11
    
    Show 6 ignore-listed frame(s)
    Array.map
    <anonymous> (0:0)
    BookCard
    src/components/catalog/book-card.tsx (43:11)
    <unknown>
    src/app/(authenticated)/catalog/page.tsx (252:13)
    Array.map
    <anonymous> (0:0)
    CatalogPage
    src/app/(authenticated)/catalog/page.tsx (251:26)

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
