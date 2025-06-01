8e6b9ea49c2c424839d2e39b63feeb5e4305a523
Yuri Lucas <yurilucassfm@gmail.com>
2025-06-01 00:15:47 +0000

    final

--- next.config.ts
+++ next.config.ts
@@ -10,19 +10,12 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    // ATENÇÃO: A configuração abaixo com `hostname: '**'` permite
-    // imagens de QUALQUER fonte. Isso pode ter implicações de segurança,
-    // pois desabilita uma camada de proteção do Next.js contra o uso
-    // do seu servidor para otimizar imagens de fontes não confiáveis.
-    // A prática recomendada é listar explicitamente apenas os domínios confiáveis.
+   
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**', // Permite qualquer hostname
+        hostname: '**', 
       },
-      // Você pode remover ou comentar os padrões específicos abaixo,
-      // pois o padrão curinga '**' já cobre todos eles.
-      // Mantendo-os aqui não causa problemas, mas são redundantes.
       {
         protocol: 'https',
         hostname: 'placehold.co',
